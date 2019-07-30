49b32a25eb0f44d2447a72a43a633b4c0f40006d
Arturs <artuurs.kupcs@gmail.com>
2019-07-30 11:38:54 +0300

    updated image service provider

--- src/ImageServiceProvider.php
+++ src/ImageServiceProvider.php
@@ -3,33 +3,39 @@
 namespace MgSoftware\Image;
 
 use Illuminate\Support\ServiceProvider;
+use MgSoftware\Image\components\ImageComponent;
 
 class ImageServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->app->singleton('image', function($app) {
+            return new ImageComponent();
+        });
     }
 
     public function boot()
     {
+        // php artisan vendor:publish --tag=mgsoftware-image
+
         // php artisan vendor:publish --tag=image-migrations
         $this->_publishMigrations();
-        // php artisan vendor:publish --tag=image-models
-        $this->_publishModels();
+
+        // php artisan vendor:publish --tag=image-config
+        $this->_publishConfiguration();
     }
 
     private function _publishMigrations()
     {
         $this->publishes([
             __DIR__.'/migrations/' => database_path('migrations')
-        ], ['migrations', 'image-migrations']);
+        ], ['migrations', 'mgsoftware-image', 'image-migrations']);
     }
 
-    private function _publishModels()
+    private function _publishConfiguration()
     {
         $this->publishes([
-            __DIR__.'/models/' => 'app/Models'
-        ], ['image-models']);
+            __DIR__.'/config/' => config_path()
+        ], ['config', 'mgsoftware-image', 'image-config']);
     }
 }
