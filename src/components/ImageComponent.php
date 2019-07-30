287b2cf586fc2093bea6872ce8215989df15781b
Arturs <artuurs.kupcs@gmail.com>
2019-07-30 11:38:15 +0300

    added and modified image and resize component

---
+++ src/components/ImageComponent.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace MgSoftware\Image\components;
+
+use GuzzleHttp\Client;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Http\FileHelpers;
+use Illuminate\Http\UploadedFile;
+use League\Flysystem\AdapterInterface;
+use MgSoftware\Image\models\Image;
+use MgSoftware\Image\models\ImageThumb;
+
+/**
+ * Class ImageComponent
+ * @property string|null $scheme
+ */
+class ImageComponent
+{
+
+    /**
+     * Storage adapter for original images
+     * @var FilesystemAdapter
+     */
+    public $originalStorage;
+
+    /**
+     * Storage adapter for thumbnails
+     * @var FilesystemAdapter
+     */
+    public $thumbStorage;
+
+
+    /**
+     * Component that is responsible for image resizing
+     * @var ResizeComponent
+     */
+    public $resize = 'MgSoftware\Image\components\ResizeComponent';
+
+    /**
+     * @var array Pre resizes images
+     */
+    public $types = [];
+
+    /**
+     * Template for building urls.
+     * To use hardcoded scheme (e.g. for console commands) set [[scheme]]
+     * parameter in components configuration.
+     * @var string
+     */
+    public $urlTemplate = '/storage/{path}';
+
+    /** @var string */
+    public $thumbPathTemplate = '/storage/{path}';
+
+    /**
+     * List of supported mime types
+     * @var array
+     */
+    public $mimeTypes = [
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'png' => 'image/png',
+        'wbmp' => 'image/vnd.wap.wbmp',
+        'xbm' => 'image/xbm',
+    ];
+
+    /**
+     * Apply parameters for original image resize
+     * @var array
+     */
+    public $originalImageParams = [
+        ResizeComponent::PARAM_WIDTH => 3840,
+        ResizeComponent::PARAM_HEIGHT => 2160,
+        ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MIN,
+        ResizeComponent::PARAM_JPEG_QUALITY => 90,
+    ];
+
+    /** @inheritdoc */
+    public function __construct()
+    {
+        $this->types = $this->getConfig('.types');
+        $this->originalStorage = \Storage::drive($this->getConfig('.storage.original'));
+        $this->thumbStorage = \Storage::drive($this->getConfig('.storage.thumb'));
+        $this->resize = new ResizeComponent();
+
+        if (!$this->originalStorage) {
+            throw new InvalidConfigException('`image` storage must be set.');
+        }
+        if (!$this->thumbStorage) {
+            throw new InvalidConfigException('`thumb` storage must be set.');
+        }
+    }
+
+    public function getConfig(string $suffix = '')
+    {
+        return config('image'.$suffix);
+    }
+
+    /**
+     * Saves image from UploadedFile instance
+     * @param UploadedFile $instance
+     * @return Image
+     * @throws \Exception
+     */
+    public function saveFromInstance(UploadedFile $instance)
+    {
+
+        // Validate mime
+        $mimeType = $instance->getMimeType();
+        if (!$mimeType || !$this->isMimeTypeValid($mimeType)) {
+            throw new \Exception('Wrong mime type. Given: '.$mimeType);
+        }
+        return $this->saveImage($instance->tempName, $mimeType);
+    }
+
+    /**
+     * Saves image from local file
+     * @param $fileName
+     * @return Image
+     * @throws \Exception|\Throwable
+     */
+    public function saveFromFile($fileName)
+    {
+        if (!file_exists($fileName)) {
+            throw new \Exception('file not exists: '.$fileName);
+        }
+        $data = file_get_contents($fileName);
+        $base64 = base64_encode($data);
+        return $this->saveFromBase64($base64);
+    }
+
+    /**
+     * Saves image from url
+     * @param $url
+     * @return Image
+     * @throws \Exception|\Throwable
+     */
+    public function saveFromUrl($url)
+    {
+        $client = new Client();
+        $response = $client->get( $url, [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+            ],
+            'curl' => [
+                CURLOPT_TIMEOUT => 60,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_SSL_VERIFYPEER => false,
+            ]
+        ]);
+        if ($response->getStatusCode() !== 200) {
+            throw new \Exception('Failed to load image contents. Code: '.$response->getStatusCode().'. Url: '.$url);
+        }
+        $base64 = base64_encode($response->getBody());
+        return $this->saveFromBase64($base64);
+    }
+
+    /**
+     * Saves image from base64 string
+     * @param $data
+     * @return Image
+     * @throws \Exception|\Throwable
+     */
+    public function saveFromBase64($data)
+    {
+        $f = finfo_open();
+        $mimeType = finfo_buffer($f, base64_decode($data), FILEINFO_MIME_TYPE);
+        unset($f);
+        if (!$mimeType || !$this->isMimeTypeValid($mimeType)) {
+            throw new \Exception('Wrong mime type. Given: '.$mimeType);
+        }
+
+        // Save image to tmp file
+        $fh = tmpfile();
+        stream_filter_append($fh, 'convert.base64-decode', STREAM_FILTER_WRITE);
+        fwrite($fh, $data);
+
+        // Save image
+        $location = stream_get_meta_data($fh)['uri'];
+        $image = $this->saveImage($location, $mimeType);
+
+        // Close handle and return image
+        fclose($fh);
+        unset($fh);
+        return $image;
+    }
+
+    /**
+     * Saves image from temp location
+     * @param string $tmpLocation Image temporary location
+     * @param $mimeType
+     * @return Image Image object
+     * @throws \Exception|\Throwable
+     */
+    protected function saveImage($tmpLocation, $mimeType)
+    {
+        return \DB::transaction(function () use ($tmpLocation, $mimeType) {
+            // Save image with temporary state
+            $image = new Image();
+            $image->path = $image->filename = 'temp';
+            $image->mime_type = $mimeType;
+            $image->height = 0;
+            $image->width = 0;
+            $image->saveOrFail();
+
+            // Generate upload path and filename
+            $extension = $this->getExtensionByMimeType($mimeType);
+            $image->filename = $image->id.'.'.$extension;
+            $image->path = $this->generateUploadPath($image->id).'/'.$image->filename;
+
+            // Save content
+            $content = $this->resize->thumbFromFile($tmpLocation, $extension, $this->originalImageParams);
+            $this->originalStorage->put($image->path, $content, [
+                'visibility' => AdapterInterface::VISIBILITY_PRIVATE
+            ]);
+
+            // Set image size and save image
+            list($width, $height) = getimagesizefromstring($content);
+            $image->height = $height;
+            $image->width = $width;
+            $image->saveOrFail();
+
+            foreach (array_keys($this->types) as $type) {
+                $this->createThumb($image, $content, $type);
+            }
+
+            return $image;
+        });
+    }
+
+    public function createThumb(Image $image, $originalContent, $type)
+    {
+        \DB::transaction(function () use ($image, $originalContent, $type) {
+            $params = $this->types[$type];
+
+            $extension = $this->getExtensionByMimeType($image->mime_type);
+            $content = $this->resize->thumbFromContent($originalContent, $extension, $params);
+            $hash = md5($image->id.$type.microtime());
+
+            $model = new ImageThumb(ImageThumb::buildAttributes($params));
+            $model->image_id = $image->id;
+            $model->type = $type;
+            $model->path = $this->generateUploadPath($image->id).'/'.$hash.'/'.$image->filename;
+            $model->saveOrFail();
+
+            $this->thumbStorage->put($model->path, $content, [
+                'visibility' => AdapterInterface::VISIBILITY_PUBLIC
+            ]);
+        });
+    }
+
+    /**
+     * Returns public url of image
+     * @param string $path
+     * @return string
+     */
+    public function getImageUrl($path)
+    {
+        $directory = dirname($path);
+        $filename = basename($path);
+        $url = strtr($this->urlTemplate, [
+            '{path}' => $path,
+            '{directory}' => $directory,
+            '{filename}' => $filename,
+        ]);
+        return $url;
+    }
+
+    /**
+     * Returns extension by mime type
+     * @param $mimeType
+     * @return string|boolean
+     */
+    public function getExtensionByMimeType($mimeType)
+    {
+        return array_search($mimeType, $this->mimeTypes);
+    }
+
+    /**
+     * Checks if mime type is valid
+     * @param $mimeType
+     * @return bool
+     */
+    public function isMimeTypeValid($mimeType)
+    {
+        return in_array($mimeType, $this->mimeTypes);
+    }
+
+    /**
+     * This is native php function wrapper, native function raises error.
+     * @param $fileName
+     * @param null $imageInfo
+     * @return array
+     * @throws \Exception
+     */
+    public function getImageSize($fileName, &$imageInfo = null)
+    {
+        $data = @getimagesize($fileName, $imageInfo);
+        if ($data === false) {
+            throw new \Exception('Failed to get image size.');
+        }
+        return [
+            'width' => (int) $data[0],
+            'height' => (int) $data[1],
+            'mimeType' => $data['mime'],
+        ];
+    }
+
+    /**
+     * Returns base path for image
+     * @param $id
+     * @return string
+     */
+    protected function generateUploadPath($id)
+    {
+        $parentFolder = date('Ym', time());
+
+        $leadingZeros = str_pad($id, 6, '0', STR_PAD_LEFT);
+        $folder1 = substr($leadingZeros, 0, 3);
+        $folder2 = substr($leadingZeros, 3);
+        $path = implode('/', [$parentFolder, $folder1, $folder2]);
+        $path = trim($path, '/');
+
+        return $path;
+    }
+}
---
+++ src/components/ResizeComponent.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace MgSoftware\Image\components;
+
+use Imagine\Image\ImageInterface as ImagineInterface;
+use Imagine\Image\Palette\RGB;
+use Imagine\Image\Point;
+use Imagine\Imagick\Image as ImagineImage;
+use Imagine\Imagick\Imagine;
+use yii\helpers\FileHelper;
+
+class ResizeComponent
+{
+    // Parameters
+    const PARAM_WIDTH = 'w';
+    const PARAM_HEIGHT = 'h';
+    const PARAM_JPEG_QUALITY = 'q';
+    const PARAM_RATIO = 'r';
+    const PARAM_BLUR = 'b';
+    const PARAM_NO_ZOOM_IN = 'nz';
+    const PARAM_CROP = 'c';
+    const PARAM_BACKGROUND = 'bg';
+    const PARAM_NORMALIZE = 'n';
+    const PARAM_AUTO_GAMMA = 'ag';
+
+    const RATIO_MIN = 'min'; // any of sides is not larger than specified
+    const RATIO_MAX = 'max'; // any of sides is equal or larger than specified (Images are not zoomed in, if they are smaller)
+
+    /** @var array Default parameters */
+    public $defaultParameters = [
+        self::PARAM_JPEG_QUALITY => 75,
+        self::PARAM_RATIO => self::RATIO_MAX,
+        self::PARAM_CROP => 0,
+        self::PARAM_NO_ZOOM_IN => 1,
+    ];
+
+    /**
+     * Performs image resize and returns image content
+     * @param $originalFile
+     * @param $extension
+     * @param $params
+     * @return string
+     */
+    public function thumbFromFile($originalFile, $extension, $params)
+    {
+        $original = (new Imagine())->open($originalFile);
+        return $this
+            ->createImage($original, $params, $options)
+            ->get($extension, $options);
+    }
+
+    /**
+     * Performs image resize and returns image content
+     * @param $content
+     * @param $extension
+     * @param $params
+     * @return string
+     */
+    public function thumbFromContent($content, $extension, $params)
+    {
+        $original = (new Imagine())->load($content);
+        return $this
+            ->createImage($original, $params, $options)
+            ->get($extension, $options);
+    }
+
+    /**
+     * Calculates ratio based on parameters
+     * @param int $originalWidth
+     * @param int $originalHeight
+     * @param array $params
+     * @return float
+     */
+    public static function calculateRatio(int $originalWidth, int $originalHeight, array $params): float
+    {
+        // Calculate image ratios
+        $ratios = [];
+        if (isset($params[static::PARAM_WIDTH])) {
+            $ratios[] = (int) $params[static::PARAM_WIDTH] / $originalWidth;
+        }
+        if (isset($params[static::PARAM_HEIGHT])) {
+            $ratios[] = (int) $params[static::PARAM_HEIGHT] / $originalHeight;
+        }
+        if (!$ratios) {
+            return 1;
+        }
+
+        // Choose ratio by ratio algorithm
+        if (isset($params[static::PARAM_RATIO]) && $params[static::PARAM_RATIO] == static::RATIO_MIN) {
+            $ratio = min($ratios);
+        } else {
+            $ratio = max($ratios);
+        }
+
+        // No zoom in image
+        $noZoomIn = !isset($params[static::PARAM_NO_ZOOM_IN]) || $params[static::PARAM_NO_ZOOM_IN];
+        if ($noZoomIn && $ratio >= 1) {
+            return 1;
+        }
+        return $ratio;
+    }
+
+    public function mergeParamsWithDefault($params)
+    {
+        return array_merge($this->defaultParameters, $params);
+    }
+
+    /**
+     * Creates image instance from given parameters
+     * @param $originalFile
+     * @param $params
+     * @param $options
+     * @return ImagineImage
+     */
+    protected function createImage(ImagineImage $original, $params, &$options)
+    {
+        $params = $this->mergeParamsWithDefault($params);
+        if (isset($params[static::PARAM_BACKGROUND])) {
+            $color = (new RGB())->color($params[static::PARAM_BACKGROUND]);
+            $image = (new Imagine())->create($original->getSize(), $color);
+            $image->paste($original, new Point(0, 0));
+        } else {
+            $image = $original;
+        }
+
+        if (isset($params[static::PARAM_NORMALIZE]) && $params[static::PARAM_NORMALIZE]) {
+            $image->getImagick()->normalizeImage();
+        }
+
+        if (isset($params[static::PARAM_AUTO_GAMMA]) && $params[static::PARAM_AUTO_GAMMA]) {
+            $image->getImagick()->autoGammaImage();
+        }
+
+        // Build options for image processing
+        $options = [];
+        if (isset($params[static::PARAM_JPEG_QUALITY])) {
+            $options['jpeg_quality'] = $params[static::PARAM_JPEG_QUALITY];
+        }
+        if ($image->layers()->count() > 1) {
+            $options['animated'] = true;
+        }
+
+        // Crop image
+        if (isset($params[static::PARAM_CROP]) && $params[static::PARAM_CROP] && isset($params[static::PARAM_WIDTH], $params[static::PARAM_HEIGHT])) {
+            $image->getImagick()->cropThumbnailImage((int) $params[static::PARAM_WIDTH], (int) $params[static::PARAM_HEIGHT]);
+        } // Resize image
+        else if (isset($params[static::PARAM_WIDTH]) || isset($params[static::PARAM_HEIGHT])) {
+            $this->resize($image, $params);
+        }
+
+        // Blur image
+        if (isset($params[static::PARAM_BLUR])) {
+            $blur = (int) $params[static::PARAM_BLUR];
+            $image->effects()->blur($blur);
+        }
+
+        return $image;
+    }
+
+    /**
+     * Performs image resize
+     * @param ImagineImage $image
+     * @param $params
+     */
+    protected function resize(ImagineImage $image, $params)
+    {
+        // Animated gif
+        if ($image->layers()->count() > 1) {
+            $image->layers()->coalesce();
+            foreach ($image->layers() as $frame) {
+                $frame->interlace(ImagineInterface::INTERLACE_PLANE);
+                $this->resizeLayer($frame, $params);
+            }
+        } // Standard image
+        else {
+            $image->interlace(ImagineInterface::INTERLACE_PLANE);
+            $this->resizeLayer($image, $params);
+        }
+    }
+
+    /**
+     * Resize image layer
+     * @param ImagineImage $image
+     * @param $params
+     * @return ImagineImage
+     */
+    protected function resizeLayer(ImagineImage $image, $params)
+    {
+        $imageSize = $image->getSize();
+        $ratio = static::calculateRatio($imageSize->getWidth(), $imageSize->getHeight(), $params);
+        if ($ratio === 1) {
+            return $image;
+        }
+
+        // Resize image
+        $box = $imageSize->scale($ratio);
+        return $image->resize($box);
+    }
+}
