ccc8735b462f9fc0f673fa5b79733c52456d6b37
rmezulis <r.mezulis@gmail.com>
2020-08-04 12:22:18 +0300

    Added previous Image types

--- src/components/ImageType.php
+++ src/components/ImageType.php
@@ -10,26 +10,25 @@ class ImageType
     const TYPE_MEDIUM = 200;
     const TYPE_SMALL = 300;
     const TYPE_SMALL_BLURRED = 400;
-    const TYPE_FULLSCREEN = 500;
 
     public static $params = [
         self::TYPE_LARGE => [
             ResizeComponent::PARAM_WIDTH => 1200,
             ResizeComponent::PARAM_HEIGHT => 800,
-            ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MAX,
+            ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MIN,
             ResizeComponent::PARAM_NORMALIZE => 1,
             ResizeComponent::PARAM_JPEG_QUALITY => 90,
         ],
         self::TYPE_MEDIUM => [
-            ResizeComponent::PARAM_WIDTH => 800,
-            ResizeComponent::PARAM_HEIGHT => 540,
+            ResizeComponent::PARAM_WIDTH => 600,
+            ResizeComponent::PARAM_HEIGHT => 400,
             ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MAX,
             ResizeComponent::PARAM_NORMALIZE => 1,
             ResizeComponent::PARAM_JPEG_QUALITY => 90,
         ],
         self::TYPE_SMALL => [
-            ResizeComponent::PARAM_WIDTH => 420,
-            ResizeComponent::PARAM_HEIGHT => 280,
+            ResizeComponent::PARAM_WIDTH => 300,
+            ResizeComponent::PARAM_HEIGHT => 200,
             ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MAX,
             ResizeComponent::PARAM_NORMALIZE => 1,
             ResizeComponent::PARAM_JPEG_QUALITY => 90,
@@ -39,11 +38,6 @@ class ImageType
             ResizeComponent::PARAM_HEIGHT => 40,
             ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MAX,
             ResizeComponent::PARAM_BLUR => 5,
-        ],
-        self::TYPE_FULLSCREEN => [
-            ResizeComponent::PARAM_WIDTH => 1920,
-            ResizeComponent::PARAM_HEIGHT => 1080,
-            ResizeComponent::PARAM_RATIO => ResizeComponent::RATIO_MIN,
         ],
     ];
 }
